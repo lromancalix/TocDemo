e0e323078f37c538d470d1af33d1d386b6d6a0d8
calix <roman.calix.net@gmail.com>
2021-01-04 23:55:10 -0600

    onboarding 1.0.1

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooteroneComponent } from './footerone/footerone.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooteroneComponent],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    FooteroneComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/components/footerone/footerone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footerone',
+  templateUrl: './footerone.component.html',
+  styleUrls: ['./footerone.component.css']
+})
+export class FooteroneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
