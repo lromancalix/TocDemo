51f5e6ebb59f85061ae344edaff7c9a57d75ffa7
calix <roman.calix.net@gmail.com>
2021-01-04 19:54:36 -0600

    onboarding antes de angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { OnboardingComponent } from './pages/onboarding/onboarding.component';
+import { ValidacionesComponent } from './pages/validaciones/validaciones.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'onboarding',
+    component: OnboardingComponent
+  },
+  {
+    path: 'validaciones',
+    component: ValidacionesComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OnboardingComponent } from './pages/onboarding/onboarding.component';
+import { ValidacionesComponent } from './pages/validaciones/validaciones.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    OnboardingComponent,
+    ValidacionesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
