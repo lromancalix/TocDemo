cb6fa5df5fdf053b700f5d693dfd8877a70d37a1
calix <roman.calix.net@gmail.com>
2021-01-11 23:56:40 -0600

    Pag Validaciones

--- src/app/pages/validaciones/validaciones.component.ts
+++ src/app/pages/validaciones/validaciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-validaciones',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidacionesComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      nombre: ['', [Validators.required]],
+      app: ['', [Validators.required]],
+      apm: ['', [Validators.required]]
+    });
+  }
+
+  get EsNombreValido() {
+    return this.form.get('nombre').invalid && this.form.get('nombre').touched;
+  }
+  get EsAppValido() {
+    return this.form.get('app').invalid && this.form.get('app').touched;
+  }
+  get EsApmValido() {
+    return this.form.get('apm').invalid && this.form.get('apm').touched;
+  }
+
+  Guardar() {
+    if ( ! this.form.valid ) {
+      console.log('Formulario invalido');
+      return;
+    }
+
+    console.log("Guardado");
   }
 
 }
