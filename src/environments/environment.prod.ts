602a8db52ab96671b9d840ee9675103ffa5e58cd
Luis <roman.calix.net@gmail.com>
2021-02-21 13:50:51 -0600

    probando server godday

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,15 @@ export const environment = {
   production: true,
   //urlTocService: "/api/TOC"
   // urlTocService: "http://dtecnoweb.net/api/TOC",
-  urlTocService: "http://localhost/Notaria.SERVICE/toc_service/get_token",
-  urlOnboarding: "http://localhost/Notaria.SERVICE/toc_service/save_onboarding",
-  urlRostroVsToken: " http://localhost/Notaria.SERVICE/toc_service/rostro_vs_token_web"
-  //urlTocService: "/session-manager/v1/session-id"
+  
+  //<clx: funcional en local>
+  //urlTocService: "http://localhost/Notaria.SERVICE/toc_service/get_token",
+  //urlOnboarding: "http://localhost/Notaria.SERVICE/toc_service/save_onboarding",
+  //urlRostroVsToken: " http://localhost/Notaria.SERVICE/toc_service/rostro_vs_token_web"
+  //</clx>
+
+  urlTocService: "http://dtecnoweb.net/toc_service/get_token",
+  urlOnboarding: "http://dtecnoweb.net/toc_service/save_onboarding",
+  urlRostroVsToken: "http://dtecnoweb.net/toc_service/rostro_vs_token_web"
+  
 };
